78b808f5554330fd0093655fb6e80c6ea808b279
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-16 04:13:00 -0300

    Cronômetro Codeso

---
+++ script.js
@@ -0,0 +1,81 @@
+//Variáveis de Ambiente
+
+var quandoVamosNosVerHora = 9
+var quandoVamosNosVerDia = 18
+var quandoVamosNosVerMes = 12
+
+var atualizacaoDia = 16
+var atualizacaoMes = 12
+
+//-----------------------------//
+
+//Footer
+
+document.getElementById('squando').innerHTML = quandoVamosNosVerDia + '/' + quandoVamosNosVerMes
+document.getElementById('satualizado').innerHTML = atualizacaoDia + '/' + atualizacaoMes
+
+//-----------------------------//
+setInterval(() => {
+var data = new Date()
+
+var ms = data.getMilliseconds()
+var segundo = data.getSeconds()
+var minuto = data.getMinutes()
+var hora = data.getHours()
+var dia = data.getDate()
+var mes = data.getMonth() + 1
+//console.log(ms)
+//console.log(segundo)
+//console.log(minuto)
+//console.log(hora)
+//console.log(dia)
+//console.log(mes)
+
+
+
+//var contaMes = quandoVamosNosVerMes - mes
+
+var contaDia = quandoVamosNosVerDia - dia
+if(contaDia < 10){
+    document.getElementById('dias').innerHTML = '0' + contaDia
+}else{
+    document.getElementById('dias').innerHTML = contaDia
+}
+
+var contaHora = quandoVamosNosVerHora - 1 - hora
+if(contaHora < 10){
+    document.getElementById('horas').innerHTML = '0' + contaHora
+}else{
+    document.getElementById('horas').innerHTML = contaHora
+}
+
+var contaMinuto = 60 - 1 - minuto
+if(contaMinuto < 10){
+    document.getElementById('minutos').innerHTML = '0' + contaMinuto
+}else{
+    document.getElementById('minutos').innerHTML = contaMinuto
+}
+
+var contaSegundo = 60 - 1 - segundo
+if(contaSegundo < 10){
+    document.getElementById('segundos').innerHTML = '0' + contaSegundo
+}else{
+    document.getElementById('segundos').innerHTML = contaSegundo
+}
+
+var contaMs = 1000 - 1 - ms
+if(contaMs < 100){
+    document.getElementById('ms').innerHTML = '0' + contaMs
+}else if(contaMs <10){
+    document.getElementById('ms').innerHTML = '00' + contaMs
+}
+else{
+    document.getElementById('ms').innerHTML = contaMs
+}
+
+
+//console.log(contaMes)
+//console.log(contaDia)
+//console.log(contaHora)
+},1)
+
